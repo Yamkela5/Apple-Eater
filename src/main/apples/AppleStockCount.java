cd39845a66ac1e577f7a1114a750b798525375aa
Yamkela5 <owamjames1@gmail.com>
2019-07-17 11:19:59 +0200

    adding apples project

---
+++ src/main/apples/AppleStockCount.java
@@ -0,0 +1,63 @@
+package main.apples;
+
+public class AppleStockCount {
+  public static void main(String[] apples) {
+
+    if (apples.length == 0) {
+      System.out.println("Please give me a qty!");
+      return;
+    }
+
+    int qty = Integer.parseInt(apples[0]);
+
+    if (apples.length == 1){
+      System.out.println("Please give me the price!");
+      return;
+    }
+
+    double cost =  4.50;;
+
+    double total;
+    int dozen = 0;
+    total = qty * cost;
+
+    StringBuilder message = new StringBuilder();
+    message.append("You have bought ");
+
+      if(dozen < 1){
+        message.append(qty);
+
+        message.append(" of");
+        message.append(" apples");
+        if (qty > 1) {
+          message.append("s");
+        }
+        message.append(" for a total cost of ");
+        message.append(String.format("%.2f",total));
+        message.append(".");
+      }
+
+      else {
+        message.append(dozen);
+        message.append(" dozen");
+        if(dozen > 1){
+          message.append("s");
+        }
+
+        message.append(" of");
+        message.append(" apple");
+        if (qty > 1) {
+          message.append("s");
+        }
+        message.append(" for a total cost of ");
+        message.append(String.format("%.2f",total));
+        message.append(".");
+
+      }
+      System.out.println(message.toString());
+
+    }
+
+
+  }
+
